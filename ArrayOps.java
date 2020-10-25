fdd5367da3ddb7b44bd2994d7dd3fe49d59f2d7a
HasinRaihan223 <hraihan20@stuy.edu>
2020-10-25 16:16:51 -0400

    Sum method works and test

---
+++ ArrayOps.java
@@ -0,0 +1,10 @@
+public class HW11{
+
+  public static int sum(int[] arr){
+    int total = 0;
+    for (int i = 0; i < arr.length; i++){
+      total += arr[i];
+    }
+    return total;
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Stuff{
+  public static void main(String[]args){
+    //Sum()
+    int[] arr = {2,5,9};
+    System.out.println(HW11.sum(arr));
+  }
+}
